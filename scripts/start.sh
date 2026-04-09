e13ad3ca3f7b9997be0839b678649daf80b8e80c
daFoggo <ntgiang141105@gmail.com>
2026-04-09 11:24:33 +0700

    fix: deploy railway

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,12 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Support Railway's postgresql:// prefix for SQLAlchemy + Psycopg3
+if [[ $DATABASE_URL == postgresql://* ]]; then
+  export DATABASE_URL=$(echo $DATABASE_URL | sed 's/postgresql:\/\//postgresql+psycopg:\/\//')
+  echo "Adjusted DATABASE_URL prefix for psycopg3 compatibility."
+fi
+
 echo "Running database migrations..."
 alembic upgrade head
 
